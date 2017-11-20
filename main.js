7247263d6e21181c3efb2b580b7b5e2d2e00f0db
alexionita95 <ionita.alexandru95@gmail.com>
2017-11-20 17:39:52 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -7,26 +7,50 @@ class PlayerSquare{
 	this.context=context;
 	this.value=value;
 }
+
+componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+}
+
+convertToColor(value)
+{
+	var max = Math.log2(2048);
+	var colorMax = 255;
+	var colorStart=100;
+	var colorRange=colorMax-colorStart;
+	var percentage = Math.log2(value)/(max);
+	var blue = colorStart+ Math.floor(colorRange * percentage);
+	var green = Math.floor(colorRange * (1-percentage)*1.5);
+	var red = Math.floor(colorRange * (1 - percentage)*0.5);
+	return "#"+this.componentToHex(red)+this.componentToHex(green)+this.componentToHex(blue);
+} 
+
 render(){
 	var gap=5;
 	if(this.value===0)
 	{
-		this.context.fillStyle="#E6D1B8";
+		this.context.fillStyle="#666666";
 	}
 	else{
-		this.context.fillStyle="#EE825A";
+		this.context.fillStyle=this.convertToColor(this.value);
 	}
 	this.context.fillRect(this.x+gap,this.y+gap,this.width-2*gap,this.height - 2*gap);
 	//this.context.fillStyle="#000000";
 	//this.context.stroke();
 	if(this.value!==0)
 	{
-	this.context.font = "60px Arial";
-	this.context.fillStyle = "white";
-	this.context.textAlign = "center";
-	this.context.fillText(this.value, this.x + this.width/2, this.y+this.height/2);
+		this.context.font = "60px Arial";
+		this.context.fillStyle = "white";
+		this.context.textAlign = "left";
+		var text_width = this.context.measureText(this.value).width;
+		var text_height= parseInt(this.context.font);
+		this.context.textBaseline="middle";
+		this.context.fillText(this.value, this.x + (this.width-text_width)/2, this.y+(this.height)/2);
 	}	
 }
+
+
 };
 
 var board=[
@@ -55,12 +79,12 @@ function update()
 {
 var baseY=200;
 context.clearRect(0, 0, canvas.width, canvas.height);
-context.fillStyle="#D0D1B8";
+context.fillStyle="#333333";
 context.fillRect(0,0,canvas.width,canvas.height);
-context.font = "30px Arial";
+context.font = "40px Arial";
 context.fillStyle = "white";
 context.textAlign="left"
-context.fillText("Score: "+score,10, baseY-40); 
+context.fillText("Score: "+score,10, baseY-20); 
 for(var i=0;i<len; i++)
 {
 	for(var j=0;j<len; j++)
@@ -134,18 +158,49 @@ function processLine(line)
 }
 function computeBoardLeft()
 {
-	let oldBoard=board.slice(0);
+	let oldBoard=copyBoard();
 	for(var i=0;i<len;i++)
 	{
 		board[i]=processLine(board[i]);
 	}
-	if(board!==oldBoard)
+	if(!equals(oldBoard,board))
 	{
 	if(canInsert())
 		insertRandom();
 	else
 		gameOver();
 	}
+}
+function copyBoard()
+{
+var newBoard=[
+[0,0,0,0],
+[0,0,0,0],
+[0,0,0,0],
+[0,0,0,0]
+];
+for(var i=0;i<len;i++)
+{
+	for(j=0;j<len;j++)
+	{
+		newBoard[i][j]=board[i][j];
+	}
+}
+return newBoard;
+}
+function equals(board1, board2)
+{
+	for(var i=0;i<len;i++)
+{
+	for(j=0;j<len;j++)
+	{
+		if(board1[i][j]!=board2[i][j])
+		{
+			return false;
+		}
+	}
+}
+	return true;
 }
 function insertRandom()
 {
@@ -156,7 +211,7 @@ function insertRandom()
 		console.log(i+" "+j);
 	}
 	while(board[i][j] != 0)
-	board[i][j]=2;
+	board[i][j]=1024;
 }
 function canInsert()
 {
